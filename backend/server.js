bd10d744ba50959853297e4739ffd817ce648f61
Tyla Jeffs <tylajeffs@gmail.com>
2024-04-04 12:14:53 -0500

    add database connection, mongoose, and error handling

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const forestRoutes = require("./routes/forests");
 
 //express app
@@ -18,7 +19,15 @@ app.use((req, res, next) => {
 //routes
 app.use("/api/forests", forestRoutes);
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listening on port ", process.env.PORT);
-});
+//connect to database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connect to database & listening on port ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
