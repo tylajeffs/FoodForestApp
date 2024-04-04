209ea44ce1bbd0b0ac5ca747c13fb0d4f18e331a
Tyla Jeffs <tylajeffs@gmail.com>
2024-04-04 13:20:50 -0500

    create controllers for GET one, GET all, & POST

---
+++ backend/controllers/forestController.js
@@ -0,0 +1,75 @@
+const Forest = require("../models/ForestModel");
+const mongoose = require('mongoose')
+
+//get all forests
+const getForests = async (req, res) => {
+    const forests = await Forest.find({}).sort({createdAt: -1})
+
+    res.status(200).json(forests)
+}
+
+//get a single forest
+const getForest = async (req, res) => {
+    const { id } = req.params 
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: "no such forest"})
+    }
+
+    const forest = await Forest.findById(id)
+
+    if(!forest) {
+        return res.status(404).json({error: "no such forest"})
+    }
+
+    res.status(200).json(forest)
+}
+
+//create a new forest
+const createForest = async (req, res) => {
+    const {
+        title,
+        ecoregion,
+        canopy,
+        subCanopy,
+        shrub,
+        herb,
+        groundCover,
+        underground,
+        vine,
+        fungi,
+      } = req.body;
+    
+      //add doc to database
+      try {
+        const forest = await Forest.create({
+          title,
+          ecoregion,
+          canopy,
+          subCanopy,
+          shrub,
+          herb,
+          groundCover,
+          underground,
+          vine,
+          fungi
+        });
+    
+        res.status(200).json(forest);
+      } catch (error) {
+        res.status(400).json({ error: error.message });
+      }
+}
+
+
+//delete a forest
+
+//update a forest
+
+
+//export
+module.exports = {
+    getForests, 
+    getForest,
+    createForest
+}
--- backend/routes/forests.js
+++ backend/routes/forests.js
@@ -1,52 +1,20 @@
 const express = require("express");
-const Forest = require("../models/ForestModel");
+const {
+  getForests,
+  getForest,
+  createForest
+} = require('../controllers/forestController')
 
 const router = express.Router();
 
 //GET all forests
-router.get("/", (req, res) => {
-  res.json({ mssg: "GET all forests" });
-});
+router.get("/", getForests);
 
 //GET a single forest
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "GET a single forest" });
-});
+router.get("/:id", getForest);
 
 //POST a new forest
-router.post("/", async (req, res) => {
-  const {
-    title,
-    ecoregion,
-    canopy,
-    subCanopy,
-    shrub,
-    herb,
-    groundCover,
-    underground,
-    vine,
-    fungi,
-  } = req.body;
-
-  try {
-    const forest = await Forest.create({
-      title,
-      ecoregion,
-      canopy,
-      subCanopy,
-      shrub,
-      herb,
-      groundCover,
-      underground,
-      vine,
-      fungi
-    });
-
-    res.status(200).json(forest);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createForest);
 
 //DELETE a forest
 router.delete("/:id", (req, res) => {
