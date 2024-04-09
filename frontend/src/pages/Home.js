88fcb3e3df17b77a31065638eb175e9fa5a55363
Tyla Jeffs <tylajeffs@gmail.com>
2024-04-09 15:12:01 -0500

    connect frontend to backend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+    const [forests, setForests] = useState(null)
+
+    useEffect(() => {
+        const fetchForests = async () => {
+            const response = await fetch('/api/forests')
+            const json = await response.json()
+
+            if(response.ok) {
+                setForests(json)
+            }
+        }
+        fetchForests()
+
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="forests">
+                {forests && forests.map((forest)=> (
+                    <p key={forest._id}>{forest.title}</p>
+                ))}
+            </div>
         </div>
     )
 }
