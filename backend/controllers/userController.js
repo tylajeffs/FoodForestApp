e110940499cd0796be49ce1c45648e5c977fbf1a
Tyla Jeffs <tylajeffs@gmail.com>
2024-04-18 13:22:01 -0500

    add signup method using bcrypt to hash passwords

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,14 @@ const loginUser = async (req, res) => {
 
 //sign up user
 const signUpUser = async (req, res) => {
-    res.json({mssg: 'sign up user'})
+    const {email, password} = req.body
+
+    try {
+        const user = await User.signup(email, password)
+        res.status(200).json({email, user})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 module.exports = {signUpUser, loginUser}
