20a140f6ad0558a708a977e763cb65cce3fd19f6
Tyla Jeffs <tylajeffs@gmail.com>
2024-04-24 11:24:59 -0500

    add user authentication on login using jwt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -17,7 +17,6 @@ const userSchema = new Schema({
 })
 
 //static signup method
-
 userSchema.statics.signup = async function(email, password) {
 
     //validation
@@ -42,6 +41,28 @@ userSchema.statics.signup = async function(email, password) {
     const hash = await bcrypt.hash(password, salt)
 
     const user = await this.create({email, password: hash})
+
+    return user
+}
+
+//static login method
+userSchema.statics.login = async function(email, password) {
+
+    if(!email || !password) {
+        throw Error('All fields must be filled')
+    }
+
+    const user = await this.findOne({email})
+
+    if(!user) {
+        throw Error('Incorrect email')
+    }
+
+    const match =  await bcrypt.compare(password, user.password)
+
+    if(!match) {
+        throw Error('Incorrect password')
+    }
 
     return user
 }
