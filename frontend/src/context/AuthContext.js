e4f1c04f4c1b1964309b6115505a23493985b768
Tyla Jeffs <tylajeffs@gmail.com>
2024-04-25 18:05:34 -0500

    set up backend auth context

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,27 @@
+import { createContext, userReducer } from 'react'
+
+export const AuthContext = createContext()
+
+export const authReducer = (state, action) => {
+    switch(action.type) {
+        case 'LOGIN':
+            return { user: action.payload }
+        case 'LOGOUT':
+            return { user: null }
+        default:
+            return state
+    }
+}
+
+export const AuthContextProvider = ({children}) => {
+    const [state, dispatch] = userReducer(authReducer, {
+        user: null
+    })
+    console.log('auth context state: ', state)
+
+    return (
+        <AuthContext.Provider value={{...state, dispatch}}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ForestsContextProvider } from './context/ForestContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ForestsContextProvider>
-      <App />
-    </ForestsContextProvider>
+    <AuthContextProvider>
+      <ForestsContextProvider>
+        <App />
+      </ForestsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
